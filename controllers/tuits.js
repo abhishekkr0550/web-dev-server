6c1ef0050a90578d69a64f52ce692c601259ee70
abhishekkumar <abhi.kr0550@gmail.com>
2022-04-06 22:12:39 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,52 @@
+// export default [
+//     {
+//         "_id": "123",
+//         "postedBy": {"username": "NASA"},
+//         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+//         "likes": 345
+//     },
+//     {
+//         "_id": "234",
+//         "postedBy": { "username": "NASA" },
+//         "tuit": "Ingenuity helicopter takes flight.",
+//         "likes": 346785
+//     }
+// ];
+
+export default [
+    { "name": "Elon Musk",
+        "handle": "robert_zubrin",
+        "date": "July 4th",
+        "title": "Amazing show about SpaceX mission.",
+        "image": "https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg",
+        "profile": "https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg",
+        "heading": "Countdown : Inspiration 4 - Mission to space",
+        "data": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a chance.",
+        "coment": "12",
+        "likes": 123,
+        "dislikes": 23,
+        "retuit": "102",
+        "upload": "13",
+        "_id": "1",
+        "liked":true
+    },
+    { "name": "Sundar Pichai",
+        "handle": "sundar_pichai",
+        "date": "June 14th",
+        "title": "Amazing show about Chrome.",
+        "image": "../../images/react.png",
+        "profile": "../../images/react.png",
+
+        "heading": "Journey to Alphabet",
+        "data": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a chance.",
+        "coment": "202",
+        "likes": 233,
+        "dislikes": 303,
+
+        "retuit": "204",
+        "upload": "100",
+        "_id": "2",
+        "liked": false
+
+    }
+];
